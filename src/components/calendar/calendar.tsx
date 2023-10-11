1e9e0e8dae8630ed21687660de700684ec9dfb73
OmarNashat01 <omaranashat01@gmail.com>
2023-10-11 23:46:22 +0300

    finalize dragging

--- src/components/calendar/calendar.tsx
+++ src/components/calendar/calendar.tsx
@@ -6,16 +6,21 @@ import {
     endOfMonth,
     endOfWeek,
     addMonths,
+    addWeeks,
+    subWeeks,
     eachDayOfInterval,
     eachWeekOfInterval,
+    set,
 } from 'date-fns';
 import CalendarDay from './day';
+import { ca } from 'date-fns/locale';
 
 
 
 function EventCalendar() {
     const [currentMonth, setCurrentMonth] = useState<Date>(startOfMonth(new Date()));
     const [activeWeek, setActiveWeek] = useState<number>(0);
+    const [events, setEvents] = useState<Array<any>>([]);
 
     function incrementMonth() {
         setCurrentMonth(addMonths(currentMonth, 1));
@@ -25,11 +30,38 @@ function EventCalendar() {
         setCurrentMonth(addMonths(currentMonth, -1));
     }
 
+    function onDrop(callingDay: string | null, droppedDay: Date) {
+
+        if (!droppedDay || !callingDay) return;
+
+        let r = confirm("are you sure ?")
+        if (!r) return;
+
+        let droppedWeek = startOfWeek(new Date(droppedDay))
+        let callingWeek = startOfWeek(new Date(callingDay))
+        let diffWeeks = -callingWeek.getTime() + droppedWeek.getTime();
+        diffWeeks = diffWeeks / (1000 * 3600 * 24 * 7);
+
+        let newEvents = [...events];
+        for (let event = 0; event < events.length; event++) {
+            const element = newEvents[event];
+            console.log(startOfWeek(element.date), callingWeek);
+            console.log(startOfWeek(element.date).getTime() === callingWeek.getTime());
+            // console.log(startOfWeek(element.date))
+            // console.log(callingWeek);
+            if (startOfWeek(element.date).getTime() === callingWeek.getTime()) {
+                console.log(element);
+                element.date = addWeeks(element.date, diffWeeks)
+            }
+        }
+        setEvents(newEvents);
+    }
+
     return (
         <div className='bg-gray-400 w-full'>
             {/* <h1>Event Calendar</h1> */}
             <div className='grid grid-flow-row w-full p-8 '>
-                <div className='grid grid-cols-7 h-10 text-center '>
+                <div className='grid grid-cols-7 h-10 text-center mr-10 '>
                     {eachDayOfInterval({ start: startOfWeek(currentMonth), end: endOfWeek(currentMonth) }).map((day, index) => {
                         return (
                             <div key={index} className='border border-gray-900 p-2'>
@@ -40,19 +72,30 @@ function EventCalendar() {
                 </div>
                 {eachWeekOfInterval({ start: startOfWeek(currentMonth), end: endOfMonth(currentMonth) }).map((day, index) => {
                     return (
-                        <div key={index} className={`grid grid-cols-1 sm:grid-cols-4 md:grid-cols-7 min-h-[9rem] ${index === activeWeek ? 'h-fit' : 'h-36'} transition-all duration-700`} onClick={() => setActiveWeek(index)} >
-                            {eachDayOfInterval({ start: startOfWeek(day), end: endOfWeek(day) }).map((day, index) => {
+                        <div key={index} draggable onDragOver={(e) => { e.preventDefault() }} onDragStart={(e) => e.dataTransfer.setData('day', day.toDateString())} onDrop={(e) => { onDrop(e.dataTransfer.getData('day'), day) }} onDragEnd={(e) => { e.preventDefault(); }}
+                            className={`grid grid-cols-1 sm:grid-cols-4 md:grid-cols-7 min-h-[9rem] relative mr-10 z-10 ${index === activeWeek ? 'h-fit' : 'h-36'} transition-all duration-700`}
+                            onClick={() => setActiveWeek(index)} data-date={day}>
+
+                            {eachDayOfInterval({ start: startOfWeek(day), end: endOfWeek(day) }).map((day, ind) => {
                                 return (
-                                    <CalendarDay key={index} day={day} />
+                                    <CalendarDay key={ind} day={day} events={events} setEvents={setEvents} />
                                 )
                             })}
+                            {/* copy button */}
+                            <button className='bg-slate-900 text-white p-2 m-2 rounded-lg absolute left-[100%]' onClick={() => {
+                                navigator.clipboard.writeText(JSON.stringify(events.filter((event) => startOfWeek(event.date).getTime() === startOfWeek(day).getTime())))
+                                console.log(events.filter((event) => startOfWeek(event.date).getTime() === startOfWeek(day).getTime()));
+                            }}>copy</button>
+                            <button className='bg-slate-900 text-white p-2 m-2 rounded-lg absolute left-[100%] bottom-0' onClick={() => {
+                                setEvents(events.filter((event) => startOfWeek(event.date).getTime() !== startOfWeek(day).getTime()))
+                            }}>delete</button>
                         </div>
                     )
                 })}
             </div>
-            <button className='bg-slate-900 text-white p-2 m-2 rounded-lg' onClick={incrementMonth}>next month</button>
             <button className='bg-slate-900 text-white p-2 m-2 rounded-lg' onClick={decrementMonth}>prev month</button>
-        </div>
+            <button className='bg-slate-900 text-white p-2 m-2 rounded-lg' onClick={incrementMonth}>next month</button>
+        </div >
     )
 }
 
--- src/components/calendar/day.tsx
+++ src/components/calendar/day.tsx
@@ -1,15 +1,41 @@
-import { format } from "date-fns";
+import { format, set } from "date-fns";
 
 interface CalendarDayProps {
     day: Date;
+    events: Array<any>;
+    setEvents: Function;
 }
 
 function CalendarDay(props: CalendarDayProps) {
-    const { day } = props;
+    const { day, events, setEvents } = props;
+
+    function handleKeyPress(e: any) {
+        if (e.key === 'Enter') {
+            const newEvent = {
+                id: Date.now(),
+                title: e.target.value,
+                date: day
+            }
+            setEvents([...events, newEvent])
+            e.target.value = ''
+        }
+    }
+
     return (
-        <div className='border border-gray-900 p-2 h-full overflow-hidden'>
+        <div className='border border-gray-900 p-2 h-full overflow-hidden ' data-date={day}>
             {format(day, 'dd')}
-            <p contentEditable className="py-2">edit here</p>
+            {/* <p contentEditable className="py-2">edit here</p> */}
+            <input className="py-2 w-full rounded-sm" type="text" placeholder="edit here" data-date={day} onKeyUp={handleKeyPress} />
+            <ul>
+                {events.filter((event) => event.date.getTime() === day.getTime()).map((event, index) => (
+                    <li key={index} className="shadow-md p-2 m-2 bg-gray-500  rounded-sm relative " data-date={day}>{event.title}
+                        <button className="absolute top-0 right-0 bg-red-800 px-1 rounded-md pointer-events-auto"
+                            onClick={() => { setEvents(events.filter((e) => e.id !== event.id)) }}
+                            data-date={day}
+                        >x</button>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
